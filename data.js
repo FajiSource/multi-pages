3002140aad495a38dbe71639f71ad4f40ad85b96
seed <yellyhaze000@gmail.com>
2025-03-05 05:24:45 +0800

    user

--- data.js
+++ data.js
@@ -2,31 +2,67 @@ let messages = [
     "Did you know? Drinking water can improve your skin health and help maintain body temperature.",
     "Take breaks! Sitting for long periods can negatively affect your posture and circulation."
 ];
+let accounts = [];
 
 const loadStorage = () => {
-
-    const messagesData = JSON.parse(localStorage.getItem("messages") || "[]");
-    if (messagesData.length <= 0) {
+    if (!localStorage.getItem("messages")) {
         localStorage.setItem("messages", JSON.stringify(messages));
     }
-}
+    if (!localStorage.getItem("accounts")) {
+        localStorage.setItem("accounts", JSON.stringify(accounts));
+    }
+};
 
 const get_message = () => {
     loadStorage();
-    const messagesData = JSON.parse(localStorage.getItem("messages") || "[]");
-    console.log("messages: ", messagesData);
-    return messagesData;
+    return JSON.parse(localStorage.getItem("messages") || "[]");
 };
 
 const send_message = (message) => {
     loadStorage();
     const messagesData = JSON.parse(localStorage.getItem("messages") || "[]");
-
     messagesData.push(message);
-    messagesData.forEach(elem => {
-        console.log(elem);
-    })
     localStorage.setItem("messages", JSON.stringify(messagesData));
     return "Message sent";
 };
-export { get_message, send_message };
+
+const sign_up = (fullname, email, password) => {
+    loadStorage();
+    const accountsData = JSON.parse(localStorage.getItem("accounts") || "[]");
+
+    if (accountsData.some(user => user.email === email)) {
+        return "Email already registered.";
+    }
+
+    const user = { fullname, email, password };
+    accountsData.push(user);
+    localStorage.setItem("accounts", JSON.stringify(accountsData));
+    return "Account successfully created.";
+};
+
+const sign_in = (email, password) => {
+    loadStorage();
+    const accountsData = JSON.parse(localStorage.getItem("accounts") || "[]");
+
+    const user = accountsData.find(data => data.email === email && data.password === password);
+    if (user) {
+        const userData = { email: user.email, fullname: user.fullname };
+        localStorage.setItem("user", JSON.stringify(userData));
+        document.location.href = "./index.html";
+        return userData;
+    }
+    return false;
+};
+
+const get_user = () => {
+    const user = JSON.parse(localStorage.getItem("user") || "null");
+    return user ? user : null;
+};
+
+const logout = () => {
+    localStorage.setItem("user", null);
+    document.location.href = "./signin.html";    
+}
+
+export { get_message,logout, send_message, sign_up, sign_in, get_user };
+
